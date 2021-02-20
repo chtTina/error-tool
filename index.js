f9549c1ced285d26c653439c2cbc1a3c82cf1cbb
tina.cai <tina.cai@mart.blue>
2021-02-20 15:44:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: tina.cai
+ * @Date: 2021-02-20 15:26:35
+ * @LastEditors: tina.cai
+ * @LastEditTime: 2021-02-20 15:26:42
+ */
+console.log('test')
